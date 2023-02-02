f91a879507474ce00de95c642f6aab62678fedbf
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-02-01 23:45:27 -0600

    GraphQL setup ready

--- server/app.js
+++ server/app.js
@@ -4,13 +4,17 @@ import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import http from "http";
 
-export async function startApolloServer() {
+export async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
 
+  app.get("/", (req, res) => {
+    res.send("Welcome to my Graphql API");
+  });
+
   const server = new ApolloServer({
-    typeDefs: ``,
-    resolvers: () => {},
+    typeDefs,
+    resolvers,
   });
 
   await server.start();
@@ -18,8 +22,12 @@ export async function startApolloServer() {
   app.use("/graphql", cors(), express.json(), expressMiddleware(server));
 
   await new Promise((resolve) =>
-    httpServer.listen({
-      port: 4000,
-    })
+    httpServer.listen(
+      {
+        port: 4000,
+      },
+      resolve
+    )
   );
+  console.log(`Server ready at port ${4000}`);
 }
